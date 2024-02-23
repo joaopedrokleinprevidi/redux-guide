174ffd403c9fd2ba3748a8edac3726af9da15285
* <*>
2024-02-22 21:15:40 -0300

    feat: create user reducer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // Components
 import Cart from "../cart/index";
@@ -6,18 +7,37 @@ import Cart from "../cart/index";
 // Styles
 import * as Styles from "./styles";
 
+import { loginUser, logoutUser } from "../../redux/user/actions";
+
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
+  const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+  const dispatch = useDispatch();
+
+  console.log({ currentUser });
+
   const handleCartClick = () => {
     setCartIsVisible(true);
+  };
+
+  const logarAoClicarNoBotao = () => {
+    dispatch(loginUser({ name: "Felipe", email: "felipe@rocha.com" }));
+  };
+
+  const sairAoClicarNoBotao = () => {
+    dispatch(logoutUser());
   };
 
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        {currentUser ? (
+          <div onClick={sairAoClicarNoBotao}>Sair</div>
+        ) : (
+          <div onClick={logarAoClicarNoBotao}>Login</div>
+        )}
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
